c4972106338969695985aa981f34bb921fa90feb
camtup9 <yokop92@gmail.com>
2025-07-14 21:11:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def index():
 
 @app.route("/romanize", methods=["POST"])
 def romanizer():
-    data = request.get_json()
-    thai_text = data.get("text", "")
-    if not thai_text:
-        return jsonify({"error": "Thiếu dữ liệu văn bản"}), 400
-    result = romanize(thai_text, engine="thai2rom")
-    return jsonify({"romanized": result})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    try:
+        data = request.get_json(force=True)
+        thai_text = data.get("text", "")
+        if not thai_text:
+            return jsonify({"error": "Thiếu dữ liệu văn bản"}), 400
+        result = romanize(thai_text, engine="thai2rom")
+        return jsonify({"romanized": result})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
